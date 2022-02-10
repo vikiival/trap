c75be3ab6b15c328b297dc6f286a19f0ec9d68af
Viki Val <viktorko99@gmail.com>
2022-02-10 20:58:18 +0100

    :zap: lambda has now 200 OK for empty resp

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,6 +1,13 @@
 export async function handler(event) {
   const res = event.path.split('/')[1]
 
+  if (!res) {
+    return {
+      statusCode: 200,
+      body: 'OK!'
+    }
+  }
+
   return {
     statusCode: 302,
     headers: {
