612d5c7590482fd644a131711a3bd91cccdb4e72
Viki Val <viktorko99@gmail.com>
2022-02-11 16:25:37 +0100

    Close #6

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,10 +1,11 @@
 export async function handler(event) {
   const [, res] = event.path.split('/')
+  const name = res.toLowerCase() || '404'
 
   return {
     statusCode: 302,
     headers: {
-      Location: `https://raw.githubusercontent.com/vikiival/trap/master/images/${res || '404'}.png`,
+      Location: `https://raw.githubusercontent.com/vikiival/trap/master/images/${name}.png`,
     }
   }
 }
